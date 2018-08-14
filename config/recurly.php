4ef58aab86438a0b8b5bca5c00c94795c46b4fdb
Joris van Willigen <me@jorisvanw.me>
2018-08-14 16:26:30 +0200

    Updated to laravel 5.5

--- config/recurly.php
+++ config/recurly.php
@@ -1,17 +1,20 @@
 <?php
+
 return [
-    /**
-    * Your Recurly subdomain
-    */
-    'subdomain' => env('RECURLY_SUBDOMAIN'),
 
     /**
-    * API key to connect to Recurly
-    */
-    'api_key' => env('RECURLY_API_KEY'),
+     * Your Recurly subdomain
+     */
+    'subdomain'   => env('RECURLY_SUBDOMAIN'),
 
     /**
-    * Only needed if you plan on using the Recurly JS lib.
-    */
+     * API key to connect to Recurly
+     */
+    'api_key'     => env('RECURLY_API_KEY'),
+
+    /**
+     * Only needed if you plan on using the Recurly JS lib.
+     */
     'private_key' => env('RECURLY_PRIVATE_KEY'),
+
 ];
